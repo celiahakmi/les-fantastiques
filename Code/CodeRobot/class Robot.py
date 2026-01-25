410436adc81b4b6d3cf9b8057efbe5587bfc253b
Zina Bensadok <bensadokzina964@gmail.com>
2026-01-25 15:56:29 +0100

    Ajout de la methode se déplacer en carré ainsi que d'une méthode pour afficher les coordonnées

--- Code/CodeRobot/class Robot.py
+++ Code/CodeRobot/class Robot.py
@@ -2,23 +2,35 @@ class Robot:
     def __init__(self, x, y):
         self.x = x 
         self.y = y 
-    def haut(self, x): 
+    def haut(self): 
         if self.x != 0 :
             self.x = self.x - 1 
         else: 
             print("Le robot rencontre un mur")
-    def bas(self, x): 
+    def bas(self): 
         if self.x != 5:
             self.x = self.x + 1 
         else: 
             print("Le robot rencontre un mur")
-    def droite(self, y):
+    def droite(self):
         if self.y != 5: 
             self.y = self.y + 1 
         else:
             print("Le robot rencontre un mur")
-    def gauche(self, y): 
+    def gauche(self): 
         if self.y != 0: 
             self.y = self.y - 1 
         else: 
             print("Le robot rencontre un mur")
+    def carre(self): 
+        self.droite
+        self.bas
+        self.gauche
+        self.haut
+    def afficher(self):
+        print(f"x : {self.x}, y = {self.y}")
+
+RobotTest = Robot(2,2)
+RobotTest.afficher()
+RobotTest.carre() 
+RobotTest.afficher()
