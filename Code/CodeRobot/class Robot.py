8f7641eb53b2a9220127a4af579d37cf013e80d7
Zina Bensadok <bensadokzina964@gmail.com>
2026-01-25 15:32:25 +0100

    Ajout de la méthode avancer vers le bas et modification des paramètre de la méthode haut()

--- Code/CodeRobot/class Robot.py
+++ Code/CodeRobot/class Robot.py
@@ -2,9 +2,13 @@ class Robot:
     def __init__(self, x, y):
         self.x = x 
         self.y = y 
-    def haut(self, x, y): 
+    def haut(self, x): 
         if self.x != 0 :
             self.x = self.x - 1 
         else: 
             print("Le robot rencontre un mur")
-            
+    def bas(self, x): 
+        if self.x != 5:
+            self.x = self.x + 1 
+        else: 
+            print("Le robot rencontre un mur")   
