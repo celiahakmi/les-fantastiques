6c9dee9eda80129f3567bceb0797a75f83e5ff87
ichrak33 <bchirichrak5@gmail.com>
2026-02-01 14:27:13 +0100

    Update class Robot.py

--- Code/CodeRobot/class Robot.py
+++ Code/CodeRobot/class Robot.py
@@ -1,21 +1,22 @@
 from class_plateforme import Plateforme
 
 class Robot: 
-    def __init__(self, x, y):
+    def __init__(self, x, y,plateforme):
         self.x = x 
-        self.y = y 
+        self.y = y
+        self.plateforme = plateforme 
     def haut(self): 
         if self.x != 0 :
             self.x = self.x - 1 
         else: 
             print("Le robot rencontre un mur")
     def bas(self): 
-        if self.x != (lignes - 1):
+        if self.x != (self.plateforme.lignes - 1):
             self.x = self.x + 1 
         else: 
             print("Le robot rencontre un mur")
     def droite(self):
-        if self.y != (colonnes - 1) :
+        if self.y != (self.plateforme.colonnes - 1) :
             self.y = self.y + 1 
         else:
             print("Le robot rencontre un mur")
@@ -25,7 +26,7 @@ class Robot:
         else: 
             print("Le robot rencontre un mur")
     def diag_haut_droite(self):
-        if self.x != 0  and self.y != (colonnes -1): 
+        if self.x != 0  and self.y != (self.plateforme.colonnes -1): 
             self.x = self.x - 1
             self.y = self.y + 1
         else: 
@@ -37,13 +38,13 @@ class Robot:
         else : 
             print("Le robot rencntre un mur")
     def diag_bas_droite(self):
-        if self.x != (lignes - 1) and self.y != (colonnes - 1):
+        if self.x != (self.plateforme.lignes - 1) and self.y != (self.plateforme.colonnes - 1):
             self.x = self.x + 1
             self.y = self.y + 1 
         else:
             print("Le robot rencontre un mur")
     def diag_bas_gauche(self):
-        if self.x != (lignes - 1) and self.y != 0:
+        if self.x != (self.plateforme.lignes - 1) and self.y != 0:
             self.x = self.x + 1 
             self.y = self.y - 1 
         else: 
@@ -53,61 +54,35 @@ class Robot:
     def carre(self, plateforme): 
         
         self.droite()
-        print("Nouvelle position logique du robot :")
+        print("Nouvelle position du robot :")
         self.afficher()
 
         print("\nPlateforme après déplacement :")
-        plateforme.placer_robot(robot)
+        plateforme.placer_robot(self)
         plateforme.afficher()
 
         self.bas()
-        print("Nouvelle position logique du robot :")
+        print("Nouvelle position du robot :")
         self.afficher()
 
         print("\nPlateforme après déplacement :")
-        plateforme.placer_robot(robot)
+        plateforme.placer_robot(self)
         plateforme.afficher()
 
         self.gauche()
-        print("Nouvelle position logique du robot :")
+        print("Nouvelle position  du robot :")
         self.afficher()
 
         print("\nPlateforme après déplacement :")
-        plateforme.placer_robot(robot)
+        plateforme.placer_robot(self)
         plateforme.afficher()
 
         self.haut()
-        print("Nouvelle position logique du robot :")
+        print("Nouvelle position du robot :")
         self.afficher()
 
         print("\nFin du déplacement en carré :")
-        plateforme.placer_robot(robot)
+        plateforme.placer_robot(self)
         plateforme.afficher()
 
         
-lignes = int(input("Nombre de lignes de la plateforme : "))
-colonnes = int(input("Nombre de colonnes de la plateforme : "))
-
-if lignes <0 or colonnes <0:
-    print("Les dimensions doivent etre 6positives")
-else:
-    plateforme = Plateforme(lignes, colonnes)
-
-x = int(input("Entrez la position x du robot : "))
-y = int(input("Entrez la position y du robot : "))
-
-if 0 <= x < lignes and 0 <= y < colonnes:
-    robot = Robot(x, y)   
-else:
-    print("valeurs hors plateforme ")
-   
-
-print("Position logique du robot :")
-robot.afficher()
-
-print("\nPlateforme initiale :")
-plateforme.placer_robot(robot)
-plateforme.afficher()
-
-print("Déplacement en carré...\n")
-robot.carre(plateforme)
