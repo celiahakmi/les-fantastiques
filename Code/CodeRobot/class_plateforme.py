6699d80defd01c232d162a809ca6e33ce78a3a4a
diakitekafine7 <diakitekafine@gmail.com>
2026-02-01 15:24:05 +0100

    Update class_plateforme.py

--- Code/CodeRobot/class_plateforme.py
+++ Code/CodeRobot/class_plateforme.py
@@ -3,8 +3,11 @@ class Plateforme:
         self.lignes = lignes
         self.colonnes = colonnes
         self.matrice = []
+    # placer les obstacles
+        self.obstacles = [(1, 2), (3, 3), (4, 1)]
         self.initialiser_matrice()
 
+    
     def initialiser_matrice(self):
         self.matrice = []
         for i in range(self.lignes):
@@ -12,7 +15,12 @@ class Plateforme:
             for j in range(self.colonnes):
                 ligne.append(".")
             self.matrice.append(ligne)
+    # placer les obstacles X
+        for (x, y) in self.obstacles:
+            self.matrice[x][y] = "X"
 
+    
+    # Est-ce que la plateforme est vide
     def matrice_est_vide(self):
         for ligne in self.matrice:
             for case in ligne:
@@ -20,10 +28,21 @@ class Plateforme:
                         return False
         return True
 
+    
+    # Est-ce qu'il y a un robot dans la plateforme 
+    def contient_robot(self):
+        for ligne in self.matrice:
+            for case in ligne:
+                if case == "R":
+                    return True
+        return False
+
+    
     def placer_robot(self, robot):
         self.initialiser_matrice()
         self.matrice[robot.x][robot.y] = "R"
 
+    
     def afficher(self):
         for ligne in self.matrice:
             print(" ".join(ligne))
