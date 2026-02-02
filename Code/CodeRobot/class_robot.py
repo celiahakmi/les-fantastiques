9d2a9ea2d081390b33877cdb07b39b905a8bcd09
célia <celiahakmi@gmail.com>
2026-02-02 23:07:47 +0100

    Update class_robot.py

--- Code/CodeRobot/class_robot.py
+++ Code/CodeRobot/class_robot.py
@@ -1,73 +1,110 @@
 class Robot: 
-    def __init__(self, x, y,plateforme):
+    def __init__(self, x, y,plateforme,vitesse):
         self.x = x 
         self.y = y
         self.plateforme = plateforme 
+        self.vitesse = vitesse
 
     def haut(self):
-        new_y = self.y + 1
-        if new_y < self.plateforme.hauteur and self.plateforme.est_vide(self.x, new_y):
-            self.y = new_y
-        else:
-            print("Mur ou obstacle en haut")
+        for _ in range(self.vitesse):
+            new_y = self.y + 1
+            if new_y < self.plateforme.hauteur and self.plateforme.est_vide(self.x, new_y):
+                self.y = new_y
+            else:
+                print("Mur ou obstacle en haut")
+                break
+
 
     def bas(self):
-        new_y = self.y - 1
-        if new_y >= 0 and self.plateforme.est_vide(self.x, new_y):
-            self.y = new_y
-        else:
-            print("Mur ou obstacle en bas")
+        for _ in range(self.vitesse):
+            new_y = self.y - 1
+            if new_y >= 0 and self.plateforme.est_vide(self.x, new_y):
+                self.y = new_y
+            else:
+                print("Mur ou obstacle en bas")
+                break
+
 
     def droite(self):
-        new_x = self.x + 1
-        if new_x < self.plateforme.largeur and self.plateforme.est_vide(new_x, self.y):
-            self.x = new_x
-        else:
-            print("Mur ou obstacle à droite")
+        for _ in range(self.vitesse):
+            new_x = self.x + 1
+            if new_x < self.plateforme.largeur and self.plateforme.est_vide(new_x, self.y):
+                self.x = new_x
+            else:
+                print("Mur ou obstacle à droite")
+                break
+
 
     def gauche(self):
-        new_x = self.x - 1
-        if new_x >= 0 and self.plateforme.est_vide(new_x, self.y):
-            self.x = new_x
-        else:
-            print("Mur ou obstacle à gauche")
+        for _ in range(self.vitesse):
+            new_x = self.x - 1
+            if new_x >= 0 and self.plateforme.est_vide(new_x, self.y):
+                self.x = new_x
+            else:
+                print("Mur ou obstacle à gauche")
+                break
+
 
     
     def diag_haut_gauche(self):
-        new_x = self.x - 1
-        new_y = self.y + 1
-        if new_x >= 0 and new_y < self.plateforme.hauteur and self.plateforme.est_vide(new_x, new_y):
-            self.x = new_x
-            self.y = new_y
-        else:
-            print("Mur ou obstacle en diagonale haut-gauche")
+        for _ in range(self.vitesse):
+            new_x = self.x - 1
+            new_y = self.y + 1
+            if new_x >= 0 and new_y < self.plateforme.hauteur and self.plateforme.est_vide(new_x, new_y):
+                self.x = new_x
+                self.y = new_y
+            else:
+                print("Mur ou obstacle en diagonale haut-gauche")
+                break
+
 
     def diag_haut_droite(self):
-        new_x = self.x + 1
-        new_y = self.y + 1
-        if new_x < self.plateforme.largeur and new_y < self.plateforme.hauteur and self.plateforme.est_vide(new_x, new_y):
-            self.x = new_x
-            self.y = new_y
-        else:
-            print("Mur ou obstacle en diagonale haut-droite")
+        for _ in range(self.vitesse):
+            new_x = self.x + 1
+            new_y = self.y + 1
+            if (
+                new_x < self.plateforme.largeur
+                and new_y < self.plateforme.hauteur
+                and self.plateforme.est_vide(new_x, new_y)
+            ):
+                self.x = new_x
+                self.y = new_y
+            else:
+                print("Mur ou obstacle en diagonale haut-droite")
+                break
+
 
     def diag_bas_gauche(self):
-        new_x = self.x - 1
-        new_y = self.y - 1
-        if new_x >= 0 and new_y >= 0 and self.plateforme.est_vide(new_x, new_y):
-            self.x = new_x
-            self.y = new_y
-        else:
-            print("Mur ou obstacle en diagonale bas-gauche")
+        for _ in range(self.vitesse):
+            new_x = self.x - 1
+            new_y = self.y - 1
+            if (
+                new_x >= 0
+                and new_y >= 0
+                and self.plateforme.est_vide(new_x, new_y)
+            ):
+                self.x = new_x
+                self.y = new_y
+            else:
+                print("Mur ou obstacle en diagonale bas-gauche")
+                break
+
 
     def diag_bas_droite(self):
-        new_x = self.x + 1
-        new_y = self.y - 1
-        if new_x < self.plateforme.largeur and new_y >= 0 and self.plateforme.est_vide(new_x, new_y):
-            self.x = new_x
-            self.y = new_y
-        else:
-            print("Mur ou obstacle en diagonale bas-droite")
+        for _ in range(self.vitesse):
+            new_x = self.x + 1
+            new_y = self.y - 1
+            if (
+                new_x < self.plateforme.largeur
+                and new_y >= 0
+                and self.plateforme.est_vide(new_x, new_y)
+            ):
+                self.x = new_x
+                self.y = new_y
+            else:
+                print("Mur ou obstacle en diagonale bas-droite")
+                break
+
 
             
     def afficher(self):
@@ -101,6 +138,7 @@ class Robot:
             print("\nFin du déplacement en carré :")
             self.plateforme.placer_robot(self)
             self.plateforme.afficher()
+
 
 
         
