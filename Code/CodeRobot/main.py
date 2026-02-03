5527201bd12f75f1229c97a65d26aacfe9366d5c
célia <celiahakmi@gmail.com>
2026-02-03 20:11:31 +0100

    Update main.py

--- Code/CodeRobot/main.py
+++ Code/CodeRobot/main.py
@@ -2,21 +2,21 @@ import pygame
 from class_plateforme import Plateforme
 from class_robot import Robot
 
-#l'utilisateur peut entrer le nb de lignes et colonnes qu'il souhaite
+#l'utilisateur choisit la taille de la plateforme
 lignes = int(input("Nombre de lignes : "))
 colonnes = int(input("Nombre de colonnes : "))
 
-if lignes <= 0 or colonnes <= 0:
+if lignes <= 0 or colonnes <= 0: #vérification des dimensions
     print("Dimensions invalides")
     exit()
 
-plateforme = Plateforme(lignes, colonnes)
+plateforme = Plateforme(lignes, colonnes) #création de la plateforme
 
-#  Position de départ du robot
+#position de départ du robot
 x = int(input("Position x du robot : "))
 y = int(input("Position y du robot : "))
 
-#  si le robot est bien sur la grille
+#vérification: si le robot est bien sur la grille
 if not (0 <= x < colonnes and 0 <= y < lignes):
     print("Position hors plateforme")
     exit()
@@ -26,8 +26,8 @@ vitesse = int(input("À quelle vitesse voulez-vous aller ? : "))
 
 robot = Robot(x, y, plateforme, vitesse) #création du robot
 
-# Config de Pygame 
-TAILLE = 60 # Taille d'une case en pixels
+#configuration de Pygame
+TAILLE = 60 #taille en pixels d'une case
 LARG = colonnes * TAILLE
 HAUT = lignes * TAILLE
 
@@ -36,34 +36,34 @@ fenetre = pygame.display.set_mode((LARG, HAUT))
 pygame.display.set_caption("Ma simulation de robot")
 horloge = pygame.time.Clock()
 
-# Liste des touches sont appuyées
+#ensemble des touches qui sont actuellement appuyées
 touches = set()
 
-# dessiner l'interface 
+#fonction pour dessiner l'interface à l'écran
 def dessiner():
-    fenetre.fill((255, 255, 255))  # Fond blanc
+    fenetre.fill((255, 255, 255)) #fond blanc
 
-    # Dessiner la grille (lignes et colonnes)
+    #dessiner de la grille (lignes et colonnes)
     for i in range(lignes):
         for j in range(colonnes):
             case = pygame.Rect(j*TAILLE, i*TAILLE, TAILLE, TAILLE)
             pygame.draw.rect(fenetre, (0,0,0), case, 1) # Bordure noire
 
-    # Dessiner les obstacles en noir
+    #dessin des obstacles en noir
     for (ox, oy) in plateforme.obstacles:
-        # On inverse le Y pour que le 0 soit en bas
+        #on inverse le Y pour que le 0 soit en bas
         ligne_obs = plateforme.lignes - 1 - oy
         mur = pygame.Rect(ox*TAILLE, ligne_obs*TAILLE, TAILLE, TAILLE)
         pygame.draw.rect(fenetre, (0,0,0), mur)
 
-    # Dessiner un rond qui represente le robot 
+    #dessin d'un rond représentant le robot
     y_robot = (plateforme.lignes - 1 - robot.y) * TAILLE
     robot_rond = pygame.Rect(robot.x*TAILLE + 5, y_robot + 5, TAILLE - 10, TAILLE - 10)
     pygame.draw.ellipse(fenetre, (255,0,0), robot_rond)
 
-    pygame.display.flip() # Mettre à jour l'image
+    pygame.display.flip() #mis à jour de l'écran
 
-#  Boucle principale 
+#boucle principale 
 marche = True
 while marche:
     for event in pygame.event.get():
@@ -72,19 +72,19 @@ while marche:
 
         elif event.type == pygame.KEYDOWN:
             touches.add(event.key)
-            # Si on appuie sur Espace on fait le carré
+            #si on appuie sur Espace le robot fait un carré
             if event.key == pygame.K_SPACE:
                 actions = [robot.droite, robot.bas, robot.gauche, robot.haut]
                 for bouger in actions:
-                    bouger()     # Le robot bouge
-                    dessiner()   # On redessine tout de suite
-                    pygame.time.delay(400) # Petite pause pour voir le mouvement
+                    bouger()     #le robot bouge
+                    dessiner()   #on redesine tout de suite
+                    pygame.time.delay(400) #petite pause pour voir chaque mouvement
 
         elif event.type == pygame.KEYUP:
             if event.key in touches:
                 touches.remove(event.key)
 
-#Déplacements continus avec les flèches du clavier
+    #Déplacements avec les flèches du clavier
     if pygame.K_UP in touches:
         robot.haut()
     if pygame.K_DOWN in touches:
@@ -94,7 +94,7 @@ while marche:
     if pygame.K_RIGHT in touches:
         robot.droite()
 
-    #Déplacements pour les diagonales
+    #Déplacements pour les diagonales à l'aide des touches a,z,q,s
     if pygame.K_a in touches:
         robot.diag_haut_gauche()
         
@@ -113,5 +113,5 @@ while marche:
     dessiner()
     horloge.tick(10) #vitesse de la boucle
 
-pygame.quit() #fin 
+pygame.quit() #fin de pygame
 
