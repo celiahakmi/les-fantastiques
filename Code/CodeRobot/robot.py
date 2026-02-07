b887e62b2c147400801de454b7c18eba619b7fed
diakitekafine7 <diakitekafine@gmail.com>
2026-02-07 20:44:35 +0100

    Create robot.py

---
+++ Code/CodeRobot/robot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+class Robot:
+    def __init__(self, x, y, plateforme):
+        self.x = x             
+        self.y = y             
+        self.plateforme = plateforme 
