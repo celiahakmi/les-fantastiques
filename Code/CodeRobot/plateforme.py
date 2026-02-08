88cd4e91218c2a4f3e8ac9e034ccfa44312b05ac
ichrak33 <bchirichrak5@gmail.com>
2026-02-08 16:06:36 +0100

    Update plateforme.py
    j'ai créé un fonction verifie_position qui verifie si la position 'x,y) touche un obstaccle (retourne false si collision , true sinon)

--- Code/CodeRobot/plateforme.py
+++ Code/CodeRobot/plateforme.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-import math 
+ 
 
 class Plateforme:
     def __init__(self, taille):
         self.taille = taille
         self.fig, self.ax = plt.subplots()
+        self.obstacles = [] 
 
     def initialiser_plan(self):
         """Initialise la plateforme avec une grille"""
@@ -16,6 +17,7 @@ class Plateforme:
     def afficher_robot(self, robot):
         """Afficher le robot dans le plan -> cercle rouge"""
         self.point, = self.ax.plot(robot.y, robot.x, 'ro')
+        
     def ajouter_cercle(self, x, y, rayon):
         self.obstacles.append(("cercle", x, y, rayon))
 
@@ -24,6 +26,39 @@ class Plateforme:
 
     def ajouter_triangle(self, x, y, cote):
         self.obstacles.append(("triangle", x, y, cote))
+
+    def verifier_position(self, x, y, largeur, hauteur):
+        #Vérification des bords 
+        if x < 0 or y < 0 or x + largeur > self.taille or y + hauteur > self.taille:
+            return False
+
+        for obs in self.obstacles:
+            type_obs = obs[0]
+
+            # collision cercle 
+            if type_obs == "cercle":
+                _, ox, oy, r = obs
+                # Trouver le point sur le rectangle le plus proche du centre du cercle
+                proche_x = max(x, min(ox, x + largeur))
+                proche_y = max(y, min(oy, y + hauteur))
+                
+                # Calculer la distance entre ce point et le centre
+                dist_x = ox - proche_x
+                dist_y = oy - proche_y
+                if (dist_x**2 + dist_y**2) < r**2: 
+                    return False
+
+            #  collision carré triangle 
+            elif type_obs in ["carre", "triangle"]:
+                _, ox, oy, c = obs
+                # Vérifier si deux rectangles se touchent
+                if (x < ox + c and x + largeur > ox and 
+                    y < oy + c and y + hauteur > oy):
+                    return False
+
+        return True
+
+    
 #Test
 plateforme = Plateforme(20)
 plateforme.initialiser_plan()
