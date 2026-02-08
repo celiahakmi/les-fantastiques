e22892bd761ad3a9f16ebda4c832926bee5d8dc8
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-08 12:42:04 +0100

    Ajout de la boucle principale dans le main pour maintenir l'interface graphique du plan continu ouvert

--- Code/CodeRobot/main.py
+++ Code/CodeRobot/main.py
@@ -8,3 +8,13 @@ plateforme = Plateforme(taille=20)
 # configuration  de Pygame et la View
 pygame.init()
 view = PygameView(plateforme, TAILLE_PIXEL=40)
+# Boucle principale 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    view.dessiner()
+
+pygame.quit()
