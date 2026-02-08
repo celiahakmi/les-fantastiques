bd8280d06f84f38b0022d670f9e5221836a1a692
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-08 11:49:30 +0100

    Implémentation du code de la fonction init de la Classe pygames view

--- Code/CodeRobot/affichage.py
+++ Code/CodeRobot/affichage.py
@@ -9,6 +9,14 @@ class PygameView:
             - Plateforme (Objet : notre plan continu)
             - TAILLE_PIXEL (int : La taille d'une unité de la plateforme en pixels
         """
+        self.plateforme = plateforme   # plateforme = notre plan continu avec la grille
+        self.TAILLE_PIXEL = TAILLE_PIXEL  # Taille d'une case en pixels
+
+        # Création de la fenêtre Pygame
+        self.fenetre = pygame.display.set_mode((int(plateforme.taille * TAILLE_PIXEL), int(plateforme.taille * TAILLE_PIXEL)))
+        pygame.display.set_caption("Plateforme Continue")  # Nom de la fenêtre
+
+        self.horloge = pygame.time.Clock()  # Horloge pour contrôler le FPS (régulation du temps d'affichage)
 
 
     
