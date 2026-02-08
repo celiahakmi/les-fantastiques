8f04cf9f652083bfcca0d4dc09235e7503d0a770
ichrak33 <bchirichrak5@gmail.com>
2026-02-08 15:49:59 +0100

    Update plateforme.py
    "j'ai ajouté une fonction ajouter_cercle qui crée un obstacle circulaire et ajouter_carre qui crée un obstacle carré et une fonction ajouter_triangle qui crée un obstacle triangulaire "

--- Code/CodeRobot/plateforme.py
+++ Code/CodeRobot/plateforme.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math 
 
 class Plateforme:
     def __init__(self, taille):
@@ -15,7 +16,14 @@ class Plateforme:
     def afficher_robot(self, robot):
         """Afficher le robot dans le plan -> cercle rouge"""
         self.point, = self.ax.plot(robot.y, robot.x, 'ro')
+    def ajouter_cercle(self, x, y, rayon):
+        self.obstacles.append(("cercle", x, y, rayon))
 
+    def ajouter_carre(self, x, y, cote):
+        self.obstacles.append(("carre", x, y, cote))
+
+    def ajouter_triangle(self, x, y, cote):
+        self.obstacles.append(("triangle", x, y, cote))
 #Test
 plateforme = Plateforme(20)
 plateforme.initialiser_plan()
