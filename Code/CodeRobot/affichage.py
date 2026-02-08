6abd1ec324f7cae7fc5ba27f030680baa07d3f92
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-08 11:43:46 +0100

    Ajout documentation de la fonction init permettant d'initialiser l'interfacie graphique Pygame pour afficher la plateforme

--- Code/CodeRobot/affichage.py
+++ Code/CodeRobot/affichage.py
@@ -3,4 +3,14 @@ import pygame
 class PygameView:
     """Classe permettant d'afficher toute nos fonctions controllant le robot 
         Partie View de la méthode MVC""" 
+    def __init__(self, plateforme, TAILLE_PIXEL=30):
+        """ Initialise l'interface graphique Pygame pour afficher la plateforme.
+        Paramètres : 
+            - Plateforme (Objet : notre plan continu), 
+            - TAILLE_PIXEL (int : La taille d'une unité de la plateforme en pixels )
+        """
+
+
+    
+
 
