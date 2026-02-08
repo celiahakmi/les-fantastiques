e8d889272491de6fb98d0f0a6e4eb973ff88f8bd
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-08 12:40:40 +0100

    Implémentation de pygames dans le main

--- Code/CodeRobot/main.py
+++ Code/CodeRobot/main.py
@@ -1,3 +1,10 @@
 import pygame
 from plateforme import Plateforme
 from affichage import PygameView
+
+# Création de la  la plateforme
+plateforme = Plateforme(taille=20)
+
+# configuration  de Pygame et la View
+pygame.init()
+view = PygameView(plateforme, TAILLE_PIXEL=40)
