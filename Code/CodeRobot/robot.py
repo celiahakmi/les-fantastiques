88fe606f6deb0e2281241353f36dc6ecf1c5d022
InesAddam <inesaddam06@gmail.com>
2026-02-09 10:02:46 +0100

    ajout de la fonction tourner

--- Code/CodeRobot/robot.py
+++ Code/CodeRobot/robot.py
@@ -6,8 +6,15 @@ class Robot:
         self.y=y
         self.largeur=largeur
         self.longueur=longueur
+        self.angle = angle   # position actuelle du robot en degrés
         self.plateforme = plateforme
         
+        
+    def tourner(self, delta_angle):
+        """ pour faire tourner le robot"""
+        
+        self.angle = (self.angle + delta_angle) % 360 # de combien est ce que on veut tourner notre robot
+
         
     
 
