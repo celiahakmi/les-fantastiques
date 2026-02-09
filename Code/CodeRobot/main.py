9094177e31bd93e11c9efe04d23a5f7054ba8d90
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-09 15:36:12 +0100

    Ajout d'input pour implémenter les paramètre du robot dans la simulation

--- Code/CodeRobot/main.py
+++ Code/CodeRobot/main.py
@@ -10,6 +10,11 @@ plateforme.ajouter_cercle(4, 4, 2)
 plateforme.ajouter_carre(16, 4, 3)
 plateforme.ajouter_triangle(10, 10, 3)
 
+#Paramètre du robot
+x = float(input("Position initiale x du robot : "))
+y = float(input("Position initiale y du robot : "))
+angle = float(input("Angle initial du robot (en degrés) : "))
+
 # configuration  de Pygame et la View
 pygame.init()
 view = PygameView(plateforme, TAILLE_PIXEL=40)
