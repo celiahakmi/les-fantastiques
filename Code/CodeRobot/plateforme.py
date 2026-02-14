19c5cc162b57c08da015e88d2a2ea8a15ee28044
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-14 14:10:42 +0100

    Suppression des fonctions d'affichage inutile dans la classe plateforme (séparation de l'aspect dynamique et conceptuel

--- Code/CodeRobot/plateforme.py
+++ Code/CodeRobot/plateforme.py
@@ -1,22 +1,9 @@
-import matplotlib.pyplot as plt
 from robot import Robot
 
 class Plateforme:
     def __init__(self, taille):
         self.taille = taille
-        self.fig, self.ax = plt.subplots()
         self.obstacles = [] 
-   
-    def initialiser_plan(self):
-        """Initialise la plateforme avec une grille"""
-        self.ax.set_xlim(0, self.taille)
-        self.ax.set_ylim(0, self.taille)
-        #Ajouter une grille
-        self.ax.grid(True)
-        
-    def afficher_robot(self, robot):
-        """Afficher le robot dans le plan -> cercle rouge"""
-        self.point, = self.ax.plot(robot.y, robot.x, 'ro')
         
     def ajouter_cercle(self, x, y, rayon):
         self.obstacles.append(("cercle", x, y, rayon))
