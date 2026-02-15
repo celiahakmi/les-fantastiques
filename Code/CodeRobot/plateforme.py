785b07fea78f1c7f188675d80a340dd1a6c4e646
ichrak33 <bchirichrak5@gmail.com>
2026-02-15 16:10:58 +0100

    ajout des commentaires et adaptation de la forme

--- Code/CodeRobot/plateforme.py
+++ Code/CodeRobot/plateforme.py
@@ -1,14 +1,13 @@
-from robot import Robot
+import math
 
 class Plateforme:
     def __init__(self, taille):
-        self.taille = taille
-        self.obstacles = [] 
-   
+        self.taille = float(taille)
+        self.obstacles = []  
+
     def ajouter_rectangle(self, x, y, w, h):
         self.obstacles.append(("rect", float(x), float(y), float(w), float(h)))
-
-
+   
     def collision_rectangle(self, x, y, w, h):
         # bords
         if x < 0 or y < 0 or x + w > self.taille or y + h > self.taille:
@@ -16,23 +15,27 @@ class Plateforme:
 
         # obstacles
         for _, ox, oy, ow, oh in self.obstacles:
-            if (x < ox + ow and x + w > ox and y < oy + oh and y + h > oy):
+            if (x < ox + ow and x + w > ox and
+                y < oy + oh and y + h > oy):
                 return True
         return False
 
-    #capteur de distance
+    # Capteur distance 
     def distance_jusqua_obstacle(self, x, y, angle, max_range=10.0,step=0.02,marge=0.0):
         d = 0.0
         while d <= max_range:
             px = x + d * math.cos(angle)
             py = y + d * math.sin(angle)
-            # bords (avec marge)
+
+            # bords avec marge
             if px < marge or py < marge or px > self.taille - marge or py > self.taille - marge:
                 return d
-            # obstacles (avec marge)
+                
+            # obstacles avec marge
             for _, ox, oy, ow, oh in self.obstacles:
                 if (ox - marge) <= px <= (ox + ow + marge) and (oy - marge) <= py <= (oy + oh + marge):
                     return d
-            d += step
-        return max_range
 
+            d += step
+
+        return max_range
