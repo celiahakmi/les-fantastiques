60f32415de54ab7997a0bc15b292f22a462f60e8
célia <celiahakmi@gmail.com>
2026-02-16 13:10:53 +0100

    ajout de 2 tests pour la fonction distance_jusqua_obstacle

--- Code/CodeRobot/test.py
+++ Code/CodeRobot/test.py
@@ -8,9 +8,16 @@ plateforme = Plateforme(20)
 plateforme.ajouter_rectangle(2,2,3,3) 
 print("Obstacles : ", plateforme.obstacles) #test si l'obstacle est bien dans la plateforme
 
-#Test de la fonction collision_rectangle
+#Tests de la fonction collision_rectangle
 collision1= plateforme.collision_rectangle(0, 0, 1, 1)
 print("Collision en (0,0,1,1) ? :", collision1) #False
 
 collision2= plateforme.collision_rectangle(2, 2, 1, 1)
 print("Collision en (2,2,1,1) ? :", collision2) #True
+
+#Tests de la fonction distance_jusqua_obstacle
+distance1=plateforme.distance_jusqua_obstacle(0, 0, 0)  
+print("Distance depuis (0,0) vers la droite :", distance1)
+
+distance2= plateforme.distance_jusqua_obstacle(0, 0, 1.57)  
+print("Distance depuis (0,0) vers le haut :", distance2)
