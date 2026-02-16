00242bab79b4f5a5411f6b1ecb442afa41a18982
célia <celiahakmi@gmail.com>
2026-02-16 13:20:15 +0100

    Test et initiation du robot

--- Code/CodeRobot/test.py
+++ Code/CodeRobot/test.py
@@ -26,3 +26,17 @@ print("Distance depuis (0,0) vers la droite :", distance1)
 
 distance2= plateforme.distance_jusqua_obstacle(0, 0, 1.57)  
 print("Distance depuis (0,0) vers le haut :", distance2)
+
+
+#Test Robot
+#On vérifie que le robot est initialisé correctement
+robot= Robot(
+    x=1.0,
+    y=1.0,
+    theta_deg=0,
+    width=1.0,
+    height=1.0,
+    wheel_base=1.0,
+    plateforme=plateforme
+)
+print("Position initiale : x =", robot.x, ", y =", robot.y, ", theta =", robot.theta)
