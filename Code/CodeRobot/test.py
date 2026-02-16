0361d0c6ff063acaeed36fe4695eb40d89649dc7
célia <celiahakmi@gmail.com>
2026-02-16 13:07:38 +0100

    ajout de 2 tests pour la fonction collision_rectangle

--- Code/CodeRobot/test.py
+++ Code/CodeRobot/test.py
@@ -7,3 +7,10 @@ from affichage import PygameView
 plateforme = Plateforme(20)
 plateforme.ajouter_rectangle(2,2,3,3) 
 print("Obstacles : ", plateforme.obstacles) #test si l'obstacle est bien dans la plateforme
+
+#Test de la fonction collision_rectangle
+collision1= plateforme.collision_rectangle(0, 0, 1, 1)
+print("Collision en (0,0,1,1) ? :", collision1) #False
+
+collision2= plateforme.collision_rectangle(2, 2, 1, 1)
+print("Collision en (2,2,1,1) ? :", collision2) #True
