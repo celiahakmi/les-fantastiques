9c1dd60f34bd6e370336e0c32b1a05af85c33d0c
célia <celiahakmi@gmail.com>
2026-02-16 13:05:07 +0100

    ajout du commentaire pour le test de ajouter_rectangle

--- Code/CodeRobot/test.py
+++ Code/CodeRobot/test.py
@@ -5,5 +5,5 @@ from affichage import PygameView
 
 #Test plateforme
 plateforme = Plateforme(20)
-plateforme.ajouter_rectangle(2,2,3,3)
-print("Obstacles : ", plateforme.obstacles)
+plateforme.ajouter_rectangle(2,2,3,3) 
+print("Obstacles : ", plateforme.obstacles) #test si l'obstacle est bien dans la plateforme
