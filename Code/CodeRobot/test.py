f60edd67d9c042ec7ce1adeaca453f7220efed48
célia <celiahakmi@gmail.com>
2026-02-16 13:14:38 +0100

    Ajout de commentaires pour tout les tests de plateforme

--- Code/CodeRobot/test.py
+++ Code/CodeRobot/test.py
@@ -4,18 +4,23 @@ from robot import Robot
 from affichage import PygameView
 
 #Test plateforme
+
+#Test de la fonction ajouter_rectangle 
+#On vérifie si l'obstacle est bien présent dans la plateforme
 plateforme = Plateforme(20)
 plateforme.ajouter_rectangle(2,2,3,3) 
-print("Obstacles : ", plateforme.obstacles) #test si l'obstacle est bien dans la plateforme
+print("Obstacles : ", plateforme.obstacles) 
 
-#Tests de la fonction collision_rectangle
+#Test de la fonction collision_rectangle
+#On vérifie la collision ou non
 collision1= plateforme.collision_rectangle(0, 0, 1, 1)
 print("Collision en (0,0,1,1) ? :", collision1) #False
 
 collision2= plateforme.collision_rectangle(2, 2, 1, 1)
 print("Collision en (2,2,1,1) ? :", collision2) #True
 
-#Tests de la fonction distance_jusqua_obstacle
+#Test de la fonction distance_jusqua_obstacle
+#On vérifie la distance jusqu'au premier obstacle
 distance1=plateforme.distance_jusqua_obstacle(0, 0, 0)  
 print("Distance depuis (0,0) vers la droite :", distance1)
 
