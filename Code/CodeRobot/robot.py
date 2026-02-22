f781666e0d26c31a96b5bc1e13be66f7058cae58
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-22 14:02:28 +0100

    initialisation classe robot avec documentation fonction init

--- Code/CodeRobot/robot.py
+++ Code/CodeRobot/robot.py
@@ -1,1 +1,9 @@
-
+class Robot: 
+    def __init__(self, x : float, y : float, theta : float, 
+                 L : float, larg : float, long : float ):
+        """ initialise le robot avec 
+            - position 
+            - orientation 
+            - distance entre 2 roues (L)
+            - forme robot (largeur/ longueur)"""
+        
