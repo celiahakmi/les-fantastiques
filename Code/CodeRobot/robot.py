87b37c1d522bc98592588e2f679a9115ad1c6e13
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-22 14:26:24 +0100

    initialisation des attribut fixes du robot

--- Code/CodeRobot/robot.py
+++ Code/CodeRobot/robot.py
@@ -1,3 +1,4 @@
+import math
 class Robot: 
     def __init__(self, x : float, y : float, theta : float, 
                  L : float, larg : float, long : float ):
@@ -7,3 +8,11 @@ class Robot:
             - distance entre 2 roues (L)
             - forme robot (largeur/ longueur)"""
         
+        self.x = float(x)
+        self.y = float(y)
+        self.theta = math.radians(theta)
+        self.L = float(L)
+        self.larg = float(larg)
+        self.long = float(long)
+        
+
