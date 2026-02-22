a1fdca28e63153e300ef742b16e8dccad6aed5de
diakitekafine7 <diakitekafine@gmail.com>
2026-02-22 14:15:12 +0100

    ajout fonction init obstacle plateforme.py

--- Code/CodeRobot/plateforme.py
+++ Code/CodeRobot/plateforme.py
@@ -3,3 +3,8 @@ class Plateforme:
         self.longueur = longueur #affilié à x
         self.hauteur = hauteur #affilié à y
         self.obstacles = []
+
+    def init_obstacle(self, x: float, y:float, h: float, l: float):
+        """Construire un rectancle où le sommet bas-gauche est de coordonées x,y, de hauteur h et de largeur l"""
+        if self.est_valide(x, y, l, h):
+            self.obstacles.append(("rect", float(x), float(y), float(h), float(l)))
