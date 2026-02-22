581bcb84954fba2f7025d226ad9f4d1e6388b406
diakitekafine7 <diakitekafine@gmail.com>
2026-02-22 14:16:13 +0100

    ajout fonction est_valide plateforme.py

--- Code/CodeRobot/plateforme.py
+++ Code/CodeRobot/plateforme.py
@@ -4,7 +4,23 @@ class Plateforme:
         self.hauteur = hauteur #affilié à y
         self.obstacles = []
 
+    
     def init_obstacle(self, x: float, y:float, h: float, l: float):
         """Construire un rectancle où le sommet bas-gauche est de coordonées x,y, de hauteur h et de largeur l"""
         if self.est_valide(x, y, l, h):
             self.obstacles.append(("rect", float(x), float(y), float(h), float(l)))
+
+    
+    def est_valide(self, x: float, y: float, l: float, h: float):
+        """Renvoie True si l'obstacle est valide """
+        if x < 0 or y < 0 or (x+l) > self.longueur or (y+h) > self.hauteur:
+            return False
+        
+        for obs in self.obstacles:
+            _, ox, oy, oh, ol = obs 
+            
+            if (x < ox + ol and x + l > ox and
+                y < oy + oh and y + h > oy):
+                return False 
+                
+        return True
