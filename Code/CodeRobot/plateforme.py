99c22b80aa32faa9139274caf23616bb2d486226
diakitekafine7 <diakitekafine@gmail.com>
2026-02-22 14:13:54 +0100

    Init class plateforme

--- Code/CodeRobot/plateforme.py
+++ Code/CodeRobot/plateforme.py
@@ -1,1 +1,5 @@
-
+class Plateforme:
+    def __init__(self, longueur: float, hauteur: float):
+        self.longueur = longueur #affilié à x
+        self.hauteur = hauteur #affilié à y
+        self.obstacles = []
