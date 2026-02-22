e73d7f182c170319900dee114d3fcb32095d6f89
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-22 14:46:51 +0100

    ajout de la fonction tourner, permettant de faire au robot une rotation sur lui même

--- Code/CodeRobot/robot.py
+++ Code/CodeRobot/robot.py
@@ -19,7 +19,7 @@ class Robot:
         self.vR: float = 0.0
         self.pas: float = 0.1 
     def avancer(self):
-        """ fait avancer le robot, conditions : vL= vR"""
+        """ fait avancer le robot, condition : vL= vR"""
         v = ( self.vL + self.vR)/ 2 #calcul de la vitesse du robot
         #calcul des déplacement de x et y pendant le pas 
         delta_x = v * math.cos(self.theta) * self.pas
@@ -27,5 +27,16 @@ class Robot:
         #calcul des nouvelles positions x et y du robot
         self.x = self.x + delta_x
         self.y = self.y + delta_y
+    def tourner(self)
+        """faire tourner le robot sur lui même 
+            condition : les roues n'ont pas la même vitesse"""
+        #calcul de la vitesse angulaire
+        v_ang = ( self.vR - self.vL)/ self.L 
+        #calcul et attribution du nouvel angle du robot après rotation
+        self.theta = self.theta + ( v_ang * self.pas )
+    
+        
+
+        
 
 
