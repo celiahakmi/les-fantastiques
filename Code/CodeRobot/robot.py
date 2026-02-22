263db9999a88cbec8cdc5aa6a01df1422944eec9
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-22 14:27:57 +0100

    initialisation des attribut variables du robot

--- Code/CodeRobot/robot.py
+++ Code/CodeRobot/robot.py
@@ -7,12 +7,15 @@ class Robot:
             - orientation 
             - distance entre 2 roues (L)
             - forme robot (largeur/ longueur)"""
-        
+        #attribut fixe du robot
         self.x = float(x)
         self.y = float(y)
         self.theta = math.radians(theta)
         self.L = float(L)
         self.larg = float(larg)
         self.long = float(long)
+        #attribut variable du robot 
+        self.vL: float = 0.0
+        self.vR: float = 0.0
         
 
