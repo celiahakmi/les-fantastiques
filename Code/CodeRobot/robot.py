99038ed894630e2ec42014c64a88fa87f8288e44
Zina Bensadok <bensadokzina964@gmail.com>
2026-02-23 14:14:09 +0100

    modification fonction carré sur la rotation de 9Odegré

--- Code/CodeRobot/robot.py
+++ Code/CodeRobot/robot.py
@@ -19,25 +19,14 @@ class Robot:
         self.vR: float = 0.0
         self.pas: float = 0.1 
     def avancer(self):
-        """ fait avancer le robot, condition : vL=vR"""
-        v = (self.vL + self.vR)/ 2 #calcul de la vitesse du robot
+        """ fait avancer le robot, condition : vL= vR"""
+        v = ( self.vL + self.vR)/ 2 #calcul de la vitesse du robot
         #calcul des déplacement de x et y pendant le pas 
         delta_x = v * math.cos(self.theta) * self.pas
         delta_y = v * math.sin(self.theta) * self.pas 
-        #Sauvegarde de l'ancienne position 
-        ancien_x = self.x
-        ancien_y = self.y
         #calcul des nouvelles positions x et y du robot
         self.x = self.x + delta_x
         self.y = self.y + delta_y
-        if plateforme.collision_robot(self):
-            #si collision, on annule le mouvement
-            self.x = ancien_x
-            self.y = ancien_y
-            #on arrête les roues
-            self.vL = 0.0
-            self.vR = 0.0
-            
     def tourner(self):
         """faire tourner le robot sur lui même 
             condition : les roues n'ont pas la même vitesse"""
@@ -45,42 +34,35 @@ class Robot:
         v_ang = ( self.vR - self.vL)/ self.L 
         #calcul et attribution du nouvel angle du robot après rotation
         self.theta = self.theta + ( v_ang * self.pas )
-    def carré(self):
-        """ le robot se déplace en carré"""
-        nbcoté = 0 
-        long_coté = 2 
+    def carre(self):
+        nbcote = 0
+        long_cote = 2
         etat = "avance"
         x0 = self.x
         y0 = self.y
-        while( nbcoté != 4):
-            if (etat == "avance"):
+        angle_acc = 0.0
+
+        while nbcote < 4:
+            if etat == "avance":
                 self.vL = 1.0
                 self.vR = 1.0
-                self.avancer()
-                d = math.sqrt( (self.x - x0)**2 + (self.y - y0)**2)
-                if ( d >= long_coté): 
-                    theta0 = self.theta
-                    nbcoté += 1 
+                d = math.sqrt((self.x - x0)**2 + (self.y - y0)**2)
+                if d < long_cote:
+                    self.avancer()
+                else:
                     etat = "tourner"
-            if (etat == "tourner"):
-                self.vL = 1.0
-                self.vR = -1.0
-                self.tourner()
-                if ( self.theta - theta0 >= (math.pi / 2)):
+                    angle_acc = 0.0
+
+            elif etat == "tourner":
+                self.vL = -1.0
+                self.vR = 1.0
+                omega = (self.vR - self.vL) / self.L
+                angle_acc += abs(omega * self.pas)
+                if angle_acc < math.pi / 2:
+                    self.tourner()
+                else:
+                    nbcote += 1
+                    etat = "avance"
                     x0 = self.x
                     y0 = self.y
-                    etat = "avancer"
-
-                    
-
-                
-
-            
-
-
-            
-
-
-        
-
 
