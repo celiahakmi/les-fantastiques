9af83f96dacae668a5dd4312171481038fef6679
ichrak33 <bchirichrak5@gmail.com>
2026-02-23 15:41:29 +0100

    j'ai pris l'ancien fichier main et je l'ai adapté avec les nouveau fichiers

--- Code/CodeRobot/affichage.py
+++ Code/CodeRobot/affichage.py
@@ -1,1 +1,20 @@
+import math
+import pygame
 
+
+class PygameView:
+    def __init__(self, plateforme, robot, TAILLE_PIXEL=40):
+        self.plateforme = plateforme
+        self.robot = robot
+        self.TAILLE_PIXEL = int(TAILLE_PIXEL)
+
+        if not pygame.get_init():
+            pygame.init()
+
+        w = int(self.plateforme.longueur * self.TAILLE_PIXEL)
+        h = int(self.plateforme.hauteur * self.TAILLE_PIXEL)
+        self.fenetre = pygame.display.set_mode((w, h))
+        pygame.display.set_caption("Simulation robot diff-drive")
+        self.horloge = pygame.time.Clock()
+
+   
