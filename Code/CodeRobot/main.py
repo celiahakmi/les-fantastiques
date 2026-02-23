386e3f3c26488ee5bbf14cc9e01166d2635d55b7
célia <celiahakmi@gmail.com>
2026-02-23 16:53:18 +0100

    suppression de lignes de codes inutiles

--- Code/CodeRobot/main.py
+++ Code/CodeRobot/main.py
@@ -26,21 +26,7 @@ def main():
 
     robot = Robot(1, 1, 0, L=0.5, larg=0.5, long=0.7)
     
-    print("Plateforme :", plateforme.longueur, "x", plateforme.hauteur)
-    print("Obstacles :", plateforme.obstacles)
-    print("Robot initial position :", robot.x, robot.y, "angle :", robot.theta)
-
-    #on avance
-    robot.vL = 1.0
-    robot.vR = 1.0
-    robot.avancer()
-    print("Robot après avancer :", robot.x, robot.y)
-
-    # on tourne
-    robot.vL = 0.5
-    robot.vR = 1.0
-    robot.tourner()
-    print("Robot après tourner :", robot.x, robot.y, "angle :", robot.theta)
+    
 
 if __name__ == "__main__":
     main()
