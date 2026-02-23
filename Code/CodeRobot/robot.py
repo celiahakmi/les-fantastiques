1b80febee68cdca692d5dcf60ad0ba4fb3f8d5ff
diakitekafine7 <diakitekafine@gmail.com>
2026-02-23 13:12:29 +0100

    adapter la fonction avancer aux collisions robot.py
    Peut-être qu'il ne faut pas annuler le mouvement mais juste s'arrêter, a tester

--- Code/CodeRobot/robot.py
+++ Code/CodeRobot/robot.py
@@ -19,14 +19,25 @@ class Robot:
         self.vR: float = 0.0
         self.pas: float = 0.1 
     def avancer(self):
-        """ fait avancer le robot, condition : vL= vR"""
-        v = ( self.vL + self.vR)/ 2 #calcul de la vitesse du robot
+        """ fait avancer le robot, condition : vL=vR"""
+        v = (self.vL + self.vR)/ 2 #calcul de la vitesse du robot
         #calcul des déplacement de x et y pendant le pas 
         delta_x = v * math.cos(self.theta) * self.pas
         delta_y = v * math.sin(self.theta) * self.pas 
+        #Sauvegarde de l'ancienne position 
+        ancien_x = self.x
+        ancien_y = self.y
         #calcul des nouvelles positions x et y du robot
         self.x = self.x + delta_x
         self.y = self.y + delta_y
+        if plateforme.collision_robot(self):
+            #si collision, on annule le mouvement
+            self.x = ancien_x
+            self.y = ancien_y
+            #on arrête les roues
+            self.vL = 0.0
+            self.vR = 0.0
+            
     def tourner(self):
         """faire tourner le robot sur lui même 
             condition : les roues n'ont pas la même vitesse"""
