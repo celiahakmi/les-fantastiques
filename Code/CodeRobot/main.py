f570d625b49ea2dae82db191dad62cdb0cfa339c
célia <celiahakmi@gmail.com>
2026-02-24 22:17:32 +0100

    modification pour faire un carré+chemin défini

--- Code/CodeRobot/main.py
+++ Code/CodeRobot/main.py
@@ -11,7 +11,7 @@ def main():
     p.init_obstacle(2, 2, 2, 1)
     p.init_obstacle(5, 5, 1, 1)
 
-    r = Robot(5, 2, 0, 0.5, 0.5, 0.7)
+    r = Robot(5, 4, 0, 0.5, 0.5, 0.7, p)
     view = PygameView(p, r, 50)
 
     nbcote = 0
@@ -61,38 +61,40 @@ def main():
                 else:
                     etat="chemin"
 
+
+        else:
+            etat="chemin" 
+
         #chemin
         taille_chemin= len(chemin)
 
         if etat=="chemin" and idx_p<taille_chemin:
-            if r.reaction_obstacle(1.0)!="rien":
+            tx,ty=chemin[idx_p]
+            dx=tx-r.x
+            dy=ty-r.y
+            distance=math.sqrt(dx**2+dy**2)
+
+            if distance<0.1:
+                idx_p+=1
                 r.vL=0
                 r.vR=0
-            else:
-                tx,ty=chemin[idx_p]
-                dx=tx-r.x
-                dy=ty-r.y
-                distance=math.sqrt(dx**2+dy**2)
 
-                if distance<0.1:
-                    idx_p+=1
-                    r.vL=0
-                    r.vR=0
+            else: #angle vers le point
+                angle_cible=math.atan2(dy,dx) 
+                erreur=angle_cible-r.theta
+                erreur=(erreur +math.pi)%(2*math.pi)-math.pi
 
-                else: #angle vers le point
-                    angle_cible=math.atan2(dy,dx) 
-                    erreur=angle_cible-r.theta
-                    erreur=(erreur +math.pi)%(2*math.pi)-math.pi
-
-                    if abs(erreur)>0.1:
-                        r.vL=-0.1
-                        r.vR=0.1
-                        r.tourner()
-                    else:
-                        r.vL=0.1
-                        r.vR=0.1
-                        r.avancer()
-        
+                if abs(erreur)>0.1:
+                    r.vL=-0.1
+                    r.vR=0.1
+                    r.tourner()
+                else:
+                    r.vL=0.1
+                    r.vR=0.1
+                    r.avancer()
+        elif etat=="chemin":
+            r.vL=0
+            r.vR=0
             
         view.dessiner()
 
