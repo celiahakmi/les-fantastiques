78d07f61d5a0efb74f2556044eba96da3db2745c
diakitekafine7 <diakitekafine@gmail.com>
2026-03-16 17:20:36 +0100

    maj des imports suite à la maj des modules __init__.py

--- Fantastic5/__init__.py
+++ Fantastic5/__init__.py
@@ -2,4 +2,8 @@ from .robot import Robot
 from .plateforme import Plateforme
 from .affichage import PygameView
 
-__all__ = ["Robot", "Plateforme", "PygameView"]
+from .simulation import Robot, Plateforme
+from .strategie import AvancerDroit, Tourner
+from .Graphique.affichage import PygameView
+
+__all__ = ["Robot", "Plateforme", "AvancerDroit", "Tourner", "PygameView"]
