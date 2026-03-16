ac37d85843dfe2568fc4cac3f217b8cd0f71641d
ichrak33 <bchirichrak5@gmail.com>
2026-03-16 14:35:24 +0100

    commencer à coder la strategie step

--- Fantastic5/strategie.py
+++ Fantastic5/strategie.py
@@ -9,10 +9,24 @@ class AvancerDroit:
         self.y_prec = 0.0
 
     def start(self):
-        """Initialise les variables au moment de démarrer."""
+        """Initialise les variables au moment de démarrer"""
         self.parcouru = 0.0
         self.x_prec = self.robot.x
         self.y_prec = self.robot.y
-        # On donne la vitesse aux moteurs pour aller tout droit
+        # vitesse moteurs pour aller tout droit
         self.robot.vL = 1.0
         self.robot.vR = 1.0
+   
+    def step(self):
+        """on fait avancer le robot d'un pas et on calcule la distance"""
+        # si on a fini on coupe les moteurs et on s'arrête
+        if self.stop():
+            self.robot.vL = 0.0
+            self.robot.vR = 0.0
+            return
+
+        # sinon on déplace le robot
+        self.robot.deplacer()
+
+        
+    
