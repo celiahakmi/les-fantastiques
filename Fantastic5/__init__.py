c673ab4d7e437ebb04145a28122eed1699255a1f
diakitekafine7 <diakitekafine@gmail.com>
2026-03-16 17:21:01 +0100

    suppression des anciens import __init__.py

--- Fantastic5/__init__.py
+++ Fantastic5/__init__.py
@@ -1,7 +1,3 @@
-from .robot import Robot
-from .plateforme import Plateforme
-from .affichage import PygameView
-
 from .simulation import Robot, Plateforme
 from .strategie import AvancerDroit, Tourner
 from .Graphique.affichage import PygameView
