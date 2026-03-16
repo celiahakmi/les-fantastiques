0a7cd4b0d391b787bd2eb93d3a40f5c6aff02473
ichrak33 <bchirichrak5@gmail.com>
2026-03-16 14:04:02 +0100

    la fonction deplacer ( avancer et tourner en un seule )

--- Fantastic5/simulation.py
+++ Fantastic5/simulation.py
@@ -90,38 +90,24 @@ class Robot:
         self.vR: float = 0.0
         self.pas: float = 0.1 
                      
-    def avancer(self):
-        """ fait avancer le robot, condition : vL=vR"""
-        v = (self.vL + self.vR)/ 2 #calcul de la vitesse du robot
-        #calcul des déplacement de x et y pendant le pas 
-        delta_x = v * math.cos(self.theta) * self.pas
-        delta_y = v * math.sin(self.theta) * self.pas 
-        #Sauvegarde de l'ancienne position 
+    
+    def deplacer(self):
+        #Sauvegarde de l'ancienne position
         ancien_x = self.x
         ancien_y = self.y
-        #calcul des nouvelles positions x et y du robot
-        self.x = self.x + delta_x
-        self.y = self.y + delta_y
-        if self.plateforme.collision_robot(self):
-            #si collision, on annule le mouvement
-            self.x = ancien_x
-            self.y = ancien_y
-            #on arrête les roues
-            self.vL = 0.0
-            self.vR = 0.0
-        
-            
-    def tourner(self):
-        """faire tourner le robot sur lui même condition : les roues n'ont pas la même vitesse"""
         ancien_theta = self.theta
-        #calcul de la vitesse angulaire
-        v_ang = (self.vR - self.vL)/ self.L 
-        #calcul et attribution du nouvel angle du robot après rotation
-        self.theta = self.theta + ( v_ang * self.pas )
-        if self.plateforme.collision_robot(self):
-            self.theta = ancien_theta
-            self.vL = 0.0
-            self.vR = 0.0
+
+        #Calcul des vitesses globale et angulaire
+        v = (self.vL + self.vR) / 2.0
+        w = (self.vR - self.vL) / self.L
+
+        #Mise à jour de la position et de l'orientation
+        self.x = self.x + v * math.cos(self.theta) * self.pas
+        self.y = self.y + v * math.sin(self.theta) * self.pas
+        self.theta = self.theta + w * self.pas
+
+          
+            
        
     def carre(self):
         nbcote = 0
