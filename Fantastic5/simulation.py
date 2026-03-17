a703c73254e1136c89bf221de2d7b86cb1a94bc9
InesAddam <inesaddam06@gmail.com>
2026-03-17 10:13:46 +0100

    modification de la fonction deplcer

--- Fantastic5/simulation.py
+++ Fantastic5/simulation.py
@@ -94,6 +94,9 @@ class Robot:
         self.y = self.y + v * math.sin(self.theta) * self.pas
         self.theta = self.theta + w * self.pas
         
+        if self.plateforme.collision_robot(self):
+            self.vL = 0.0
+            self.vR = 0.0
 
     def update(self):
         """Calcule le prochain mouvement théorique sans l'appliquer"""
