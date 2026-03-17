223d9df93fce4717f4197a07c49a69c9ce30813c
diakitekafine7 <diakitekafine@gmail.com>
2026-03-17 10:22:23 +0100

    update main.py

--- source/main.py
+++ source/main.py
@@ -6,20 +6,20 @@ from Fantastic5.Graphique.affichage import PygameView
 
 def main():
     p = Plateforme(10, 10)
-    p.init_obstacle(7, 6, 2, 1)
+    p.init_obstacle(7, 1, 2, 1)
     p.init_obstacle(5, 5, 1, 1)
 
     r = Robot(5, 2, 0, 0.5, 0.5, 0.7, p)
     view = PygameView(p, r, 50)
 
     # instanciation d'une stratégie
-    strat = AvancerDroit(r, 2.0) # avancer de 2m
+    strat = AvancerDroit(r, 7.0) # avancer de 2m
     strat.start()
 
     running = True
 
     while running:
-        view.horloge.tick(100) 
+        view.horloge.tick(10) 
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -33,14 +33,10 @@ def main():
 
 
         # calcule du futur théorique (via ta nouvelle fonction dans Robot)
-        nx, ny, nt = r.update()
-
-        # définit l'action de déplacement réel
-        def action_deplacement():
-            r.x, r.y, r.theta = nx, ny, nt
+        r.update()
 
         # validation ou annulation de la plateforme
-        p.deplacer_si_possible(r, action_deplacement)
+        p.collision_robot(r)
 
         view.dessiner()
 
