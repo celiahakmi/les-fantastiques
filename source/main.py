a3c841916ae98ce37470b98d7ac1c8e6695dda9d
bensazina <bensadokzina964@gmail.com>
2026-03-18 09:15:37 +0100

    modification des import

--- source/main.py
+++ source/main.py
@@ -1,8 +1,8 @@
 import pygame
 import math
-from Fantastic5.simulation import Plateforme, Robot
-from Fantastic5.strategie import AvancerDroit, TracerCarre
-from Fantastic5.Graphique.affichage import PygameView
+from source.Fantastic5.Graphique.affichage import PygameView
+from source.Fantastic5.Simulation.simulation import Plateforme, Robot
+from source.Fantastic5.Strategie.strategie import AvancerDroit, Tourner, TracerCarre
 
 def main():
     p = Plateforme(10, 10)
