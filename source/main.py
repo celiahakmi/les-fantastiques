1d2971d20e4a2a77a5b7ec116f7fe93b2c336797
diakitekafine7 <diakitekafine@gmail.com>
2026-03-18 08:43:34 +0100

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,46 +1,42 @@
 import pygame
 import math
 from Fantastic5.simulation import Plateforme, Robot
-from Fantastic5.strategie import AvancerDroit, Tourner, TracerCarre
+from Fantastic5.strategie import AvancerDroit, TracerCarre
 from Fantastic5.Graphique.affichage import PygameView
 
 def main():
     p = Plateforme(10, 10)
-    p.init_obstacle(7, 1, 2, 1)
+    p.init_obstacle(1, 6, 2, 1)
     p.init_obstacle(5, 5, 1, 1)
 
     r = Robot(5, 2, 0, 0.5, 0.5, 0.7, p)
     view = PygameView(p, r, 50)
 
-    # instanciation d'une stratégie
-    strat = TracerCarre(r,2.0)
+    strat = TracerCarre(r, 2.0) 
     strat.start()
-
+    
+    avancer = False 
     running = True
 
     while running:
-        view.horloge.tick(10) 
+        view.horloge.tick(10)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        # On demande à la stratégie de mettre à jour les vitesses vL et vR
         if not strat.stop():
             strat.step()
         else:
-            r.vL, r.vR = 0, 0 # On s'arrête si la stratégie est finie
+            # Si le carré est finit 
+            if not avancer:
+                print("Carré terminé")
+                # On remplace la stratégie par une nouvelle
+                strat = AvancerDroit(r, 2.0) # Avance de 10m
+                strat.start()
+                avancer = True 
+            else:
+                # Si la deuxième stratégie est aussi finie
+                r.vL, r.vR = 0.0, 0.0
 
-
-        # maj position robot
         r.update()
-
-        # validation ou annulation de la plateforme
-        p.collision_robot(r)
-
         view.dessiner()
-
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
