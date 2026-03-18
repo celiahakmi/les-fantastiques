a629ab3a093d513884ce0018f7c761d37d06af00
bensazina <bensadokzina964@gmail.com>
2026-03-18 09:14:04 +0100

    modification des import

--- source/__init__.py
+++ source/__init__.py
@@ -1,5 +1,5 @@
-from .Graphique.affichage import PygameView
-from .Simulation.simulation import Plateforme, Robot
-from .Stratégie.strategie import AvancerDroit, Tourner, TracerCarre
+from .Fantastic5.Graphique.affichage import PygameView
+from .Fantastic5.Simulation.simulation import Plateforme, Robot
+from .Fantastic5.Stratégie.strategie import AvancerDroit, Tourner, TracerCarre
 
 __all__ = ["PygameView","Plateforme","Robot","AvancerDroit","Tourner","TracerCarre"]
