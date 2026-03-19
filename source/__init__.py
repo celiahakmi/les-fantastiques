0951404189fc74aca85b4fe8024736791a9f645e
ichrak33 <bchirichrak5@gmail.com>
2026-03-19 13:37:35 +0000

    correction des majuscules

--- source/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Fantastic5.Graphique.affichage import PygameView
-from .Fantastic5.Simulation.simulation import Plateforme, Robot
-from .Fantastic5.Stratégie.strategie import AvancerDroit, Tourner, TracerCarre
-
-__all__ = ["PygameView","Plateforme","Robot","AvancerDroit","Tourner","TracerCarre"]
