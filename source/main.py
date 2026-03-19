c5fad808055dcbc797574fe774bebf08c93c2d7c
ichrak33 <bchirichrak5@gmail.com>
2026-03-19 15:25:33 +0100

    enlever tout les view.horloge.tick et la bibliotheque math et aussi separer la fermuture de la fenetre et le mouvement de robot

--- source/main.py
+++ source/main.py
@@ -1,10 +1,9 @@
 import pygame
 import math
-import time
-from Fantastic5.Graphique.affichage import PygameView
+import time  
 from Fantastic5.Simulation.simulation import Plateforme, Robot
-from Fantastic5.Strategie.strategie import AvancerDroit, Tourner, TracerCarre
-
+from Fantastic5.Strategie.strategie import AvancerDroit, TracerCarre
+from Fantastic5.Graphique.affichage import PygameView
 
 def main():
     p = Plateforme(10, 10)
@@ -12,39 +11,42 @@ def main():
     p.init_obstacle(5, 5, 1, 1)
 
     r = Robot(5, 2, 0, 0.5, 0.5, 0.7, p)
+
+    strat = TracerCarre(r, 2.0)
+    strat.start()
+    avancer = False
     view = PygameView(p, r, 50)
 
-    strat = TracerCarre(r, 2.0) 
-    strat.start()
-    
-    avancer = False 
     running = True
+    mouvement = True 
 
     while running:
-        view.horloge.tick(300)
-
-        if not strat.stop():
-            strat.step()
-        else:
-            # Si le carré est finit 
-            if not avancer:
-                print("Carré terminé")
-                # On remplace la stratégie par une nouvelle
-                strat = AvancerDroit(r, 10.0) # Avance de 10m
-                strat.start()
-                avancer = True 
+        if mouvement: 
+            if not strat.stop():
+                strat.step()
             else:
-                # Si la deuxième stratégie est aussi finie
-                r.vL, r.vR = 0.0, 0.0
-        r.update()
-        time.sleep(0.01)
-            
+                if not avancer:
+                    print("Carré terminé ")
+                    # On remplace la stratégie par une nouvelle
+                    strat = AvancerDroit(r, 10.0)  # avance de 10m
+                    strat.start()
+                    avancer = True
+                else:
+                    # Si la deuxième stratégie est aussi finie
+                    r.vL, r.vR = 0.0, 0.0
+
+            # On fige le robot s'il se cogne 
+            if not r.update():
+                mouvement = False  
+
+        time.sleep(0.001)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        
+                
         view.dessiner()
+
     pygame.quit()
 
 if __name__ == "__main__":
