dd546659ff56150187879a2273bb54cf540a6dc5
diakitekafine7 <diakitekafine@gmail.com>
2026-03-21 17:51:15 +0100

    Create AdaptateurIRL.py

---
+++ source/AdaptateurIRL.py
@@ -0,0 +1,31 @@
+import math
+
+class AdaptateurReel(Adaptateur):
+    def __init__(self, robot_reel):
+        super().__init__(robot_reel)
+        # On récupère les constantes physiques depuis l'API du robot
+        self.L = self.robot.WHEEL_BASE_WIDTH 
+        self.diametre = self.robot.WHEEL_DIAMETER
+
+    def set_vitesse(self, v_lineaire, v_angulaire):
+        # 1Calcul des vitesses des roues
+        v_gauche = v_lineaire - (v_angulaire * self.L / 2)
+        v_droite = v_lineaire + (v_angulaire * self.L / 2)
+
+        # Conversion m/s en dps
+        # Rayon = diametre / 2
+        rayon = self.diametre / 2
+        dps_gauche = (v_gauche / rayon) * (180 / math.pi)
+        dps_droite = (v_droite / rayon) * (180 / math.pi)
+
+        # Envoi des commandes aux moteurs réels 
+        self.robot.set_motor_dps(self.robot.MOTOR_LEFT, dps_gauche)
+        self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, dps_droite)
+
+    def get_distance(self):
+        # Méthode déjà existante dans l'API
+        return self.robot.get_distance()
+
+    def get_position(self):
+        # Méthode déjà existante dans l'API
+        return self.robot.get_motor_position()
