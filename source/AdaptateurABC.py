75ea9910de5514da93bedb778db9ae18f751b6aa
diakitekafine7 <diakitekafine@gmail.com>
2026-03-21 17:31:13 +0100

    Create AdaptateurABC.py

---
+++ source/AdaptateurABC.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Adaptateur(ABC):
+    def __init__(self, robot):
+        self.robot = robot [cite: 412]
+
+    def set_vitesse(self, v_lineaire, v_angulaire):
+        """Initialise les vitesses des roues"""
+        pass
+
+    def get_position(self):
+        """Retourne la position actuelle des roues ou du robot"""
+        pass
+
+    def get_distance(self):
+        """Retourne la distance avec l'obstacle le plus proche"""
+        pass
