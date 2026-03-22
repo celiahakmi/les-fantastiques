efd9cd39206f9cb4d822f58fce93a6db1303dde9
Zina Bensadok <bensadokzina964@gmail.com>
2026-03-22 10:00:16 +0100

    ajout du init dans le dossier Fantastic

---
+++ source/Fantastic5/__init__.py
@@ -0,0 +1,5 @@
+from .graphique import PygameView 
+from .simulation import Plateforme, Robot 
+from .strategie import AvancerDroit, Tourner, TracerCarre, Choregraphie 
+
+__all__ = ["PygameView", "Plateforme", "Robot", "AvancerDroit", "Tourner", "TracerCarre", "Choregraphie"]
