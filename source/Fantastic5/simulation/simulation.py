b2546e5964be9aa54b15305634623f642c9904d1
Zina Bensadok <bensadokzina964@gmail.com>
2026-03-22 08:51:19 +0100

    ajout du typages dans toutes les fonctions

--- source/Fantastic5/simulation/simulation.py
+++ source/Fantastic5/simulation/simulation.py
@@ -2,15 +2,15 @@ import math
 
 class Plateforme:
     def __init__(self, longueur: float, hauteur: float):
-        self.longueur = longueur #affilié à x
-        self.hauteur = hauteur #affilié à y
-        self.obstacles = []
+        self.longueur: float = longueur #affilié à x
+        self.hauteur: float= hauteur #affilié à y
+        self.obstacles: list = []
 
     
     def init_obstacle(self, x: float, y:float, h: float, l: float):
         """Construire un rectancle où le sommet bas-gauche est de coordonées x,y, de hauteur h et de largeur l"""
         if self.est_valide(x, y, l, h):
-            self.obstacles.append(("rect", float(x), float(y), float(h), float(l)))
+            self.obstacles.append(("rect", x, y, h, l))
             return True 
         return False  
 
@@ -31,8 +31,8 @@ class Plateforme:
     def collision_robot(self, robot):
         """Renvoie True si le robot entre en collision avec un obstacle ou un mur"""
 
-        long = robot.long / 2
-        larg = robot.larg / 2
+        long: float = robot.long / 2
+        larg: float = robot.larg / 2
 
         #4 coins: avant-droit, avant-gauche, arrière-gauche, arrière-droit
         coins_relatifs = [(long, larg), (long, -larg), (-long, -larg), (-long, larg)]
@@ -40,8 +40,8 @@ class Plateforme:
         coins_reels = []
         for dx, dy in coins_relatifs:
             #Rotation des points par rapport à l'angle theta
-            cx = robot.x + (dx * math.cos(robot.theta) - dy * math.sin(robot.theta))
-            cy = robot.y + (dx * math.sin(robot.theta) + dy * math.cos(robot.theta))
+            cx: float = robot.x + (dx * math.cos(robot.theta) - dy * math.sin(robot.theta))
+            cy: float = robot.y + (dx * math.sin(robot.theta) + dy * math.cos(robot.theta))
             coins_reels.append((cx, cy))
 
         #Vérifier si un des coins sort de la plateforme ou touche un obstacle
@@ -68,13 +68,13 @@ class Robot:
             - distance entre 2 roues (L)
             - forme robot (largeur/ longueur)"""
         #attribut fixe du robot
-        self.x = float(x)
-        self.y = float(y)
-        self.theta = math.radians(theta)
-        self.L = float(L)
-        self.larg = float(larg)
-        self.long = float(long)
-        self.plateforme=plateforme
+        self.x : float = x
+        self.y : float = y
+        self.theta: float = math.radians(theta)
+        self.L : float = L 
+        self.larg : float = larg
+        self.long : float = long 
+        self.plateforme: Plateforme =plateforme
         #attribut variable du robot 
         self.vL: float = 0.0
         self.vR: float = 0.0
@@ -84,11 +84,13 @@ class Robot:
     def update(self):
         """Calcule le prochain mouvement sans l'appliquer"""
         #Sauvegarde de la position actuelle au cas où on doive annuler
-        ancien_x, ancien_y, ancien_theta = self.x, self.y, self.theta
+        ancien_x: float = self.x
+        ancien_y: float = self.y
+        ancien_theta: float = self.theta
 
         # Calcul des vitesses 
-        vitesse_lineaire = (self.vL + self.vR) / 2.0
-        vitesse_angulaire = (self.vR - self.vL) / self.L
+        vitesse_lineaire: float = (self.vL + self.vR) / 2.0
+        vitesse_angulaire: float = (self.vR - self.vL) / self.L
 
         # Calcul de la position 
         self.x += vitesse_lineaire  * math.cos(self.theta) * self.pas
