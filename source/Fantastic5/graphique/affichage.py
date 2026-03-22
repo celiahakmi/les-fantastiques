9b368122b7c59bae0e1443c2c103a047570d1c48
Zina Bensadok <bensadokzina964@gmail.com>
2026-03-22 09:14:51 +0100

    suppression du if main inutile dans le fichier affichage

--- source/Fantastic5/graphique/affichage.py
+++ source/Fantastic5/graphique/affichage.py
@@ -75,9 +75,6 @@ class PygameView:
             
         pygame.display.flip()
 
-if __name__ == "__main__":
-    print("Ceci n'est pas exécuter par import")
-    print("mais uniquement par python fichier.py")
 
            
        
