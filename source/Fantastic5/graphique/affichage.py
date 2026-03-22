f9704d83fd4ec3db8c9767fc22f8d3892face86c
Zina Bensadok <bensadokzina964@gmail.com>
2026-03-22 09:21:38 +0100

    ajout du typages dans toutes les fonctions

--- source/Fantastic5/graphique/affichage.py
+++ source/Fantastic5/graphique/affichage.py
@@ -11,11 +11,11 @@ class PygameView:
         if not pygame.get_init():
             pygame.init()
 
-        w = int(self.plateforme.longueur * self.TAILLE_PIXEL)
-        h = int(self.plateforme.hauteur * self.TAILLE_PIXEL)
-        self.fenetre = pygame.display.set_mode((w, h))
+        w: int = int(self.plateforme.longueur * self.TAILLE_PIXEL)
+        h: int = int(self.plateforme.hauteur * self.TAILLE_PIXEL)
+        self.fenetre: pygame.Surface = pygame.display.set_mode((w, h))
         pygame.display.set_caption("Simulation robot diff-drive")
-        self.horloge = pygame.time.Clock()
+        self.horloge: pygame.time.Clock = pygame.time.Clock()
 
     def to_px(self, x: float, y : float):
         return (int(x * self.TAILLE_PIXEL), int(y * self.TAILLE_PIXEL))
@@ -25,7 +25,7 @@ class PygameView:
 
         # Grille verticale
         for i in range(int(self.plateforme.longueur) + 1):
-            x = i * self.TAILLE_PIXEL
+            x: int = i * self.TAILLE_PIXEL
             pygame.draw.line(
                 self.fenetre,
                 (220, 220, 220),
@@ -34,7 +34,7 @@ class PygameView:
             )
         # Grille horizontale
         for j in range(int(self.plateforme.hauteur) + 1):
-            y = j * self.TAILLE_PIXEL
+            y: int = j * self.TAILLE_PIXEL
             pygame.draw.line(
                 self.fenetre,
                 (220, 220, 220),
@@ -46,7 +46,7 @@ class PygameView:
             if obs[0] != "rect":
                 continue
             _, x, y, hauteur, largeur = obs
-            rect = pygame.Rect(
+            rect: pygame.Rect = pygame.Rect(
                 int(x * self.TAILLE_PIXEL),
                 int(y * self.TAILLE_PIXEL),
                 int(largeur * self.TAILLE_PIXEL),
@@ -56,19 +56,19 @@ class PygameView:
             
         # Robot
         cx, cy = self.to_px(self.robot.x, self.robot.y)
-        surf_w = max(1, int(self.robot.long * self.TAILLE_PIXEL))
-        surf_h = max(1, int(self.robot.larg * self.TAILLE_PIXEL))
-        robot_surf = pygame.Surface((surf_w, surf_h), pygame.SRCALPHA)
+        surf_w: int = max(1, int(self.robot.long * self.TAILLE_PIXEL))
+        surf_h: int = max(1, int(self.robot.larg * self.TAILLE_PIXEL))
+        robot_surf : pygame.Surface = pygame.Surface((surf_w, surf_h), pygame.SRCALPHA)
         robot_surf.fill((0, 0, 255))
-        angle_deg = -math.degrees(self.robot.theta)
-        rotated = pygame.transform.rotate(robot_surf, angle_deg)
-        rect_rot = rotated.get_rect(center=(cx, cy))
+        angle_deg: float = -math.degrees(self.robot.theta)
+        rotated: pygame.Surface = pygame.transform.rotate(robot_surf, angle_deg)
+        rect_rot: pygame.Rect  = rotated.get_rect(center=(cx, cy))
         self.fenetre.blit(rotated, rect_rot.topleft)
         
         # Fleche rouge pour indiquer la direction  
-        L_fleche = max(self.robot.long, 0.2)
-        x2 = self.robot.x + L_fleche * math.cos(self.robot.theta)
-        y2 = self.robot.y + L_fleche * math.sin(self.robot.theta)
+        L_fleche: float = max(self.robot.long, 0.2)
+        x2: float = self.robot.x + L_fleche * math.cos(self.robot.theta)
+        y2: float = self.robot.y + L_fleche * math.sin(self.robot.theta)
         x2p, y2p = self.to_px(x2, y2)
         pygame.draw.line(self.fenetre, (255, 0, 0), (cx, cy), (x2p, y2p), 2)
             
