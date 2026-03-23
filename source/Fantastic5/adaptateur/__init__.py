f1c3eaa3862d7b9a7ab5071e7c3e80835848105f
ichrak33 <bchirichrak5@gmail.com>
2026-03-23 13:21:33 +0100

    creation de __init__.py

--- source/Fantastic5/adaptateur/__init__.py
+++ source/Fantastic5/adaptateur/__init__.py
@@ -1,1 +1,5 @@
+from .adaptateur import Adaptateur
+from .adaptateurIRL import AdaptateurIRL
+from .adaptateurSimu import AdaptateurSimu
 
+__all__ = ["Adaptateur", "AdaptateurIRL", "AdaptateurSimu"]
