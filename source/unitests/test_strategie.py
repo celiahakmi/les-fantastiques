4482ec3f57a18d8d2249931946d25cf70b6080c5
ichrak33 <bchirichrak5@gmail.com>
2026-03-24 22:42:53 +0100

    creation testspour strategie AvancerDroit

--- source/unitests/test_strategie.py
+++ source/unitests/test_strategie.py
@@ -1,1 +1,24 @@
+import unittest
+import math
+from Fantastic5.simulation import Plateforme, Robot
+from Fantastic5.adaptateur import AdaptateurSimu
+from Fantastic5.strategie import AvancerDroit, Tourner
+
+class TestAvancerDroit(unittest.TestCase):
+    def setUp(self):
+        self.plateforme = Plateforme(10.0, 10.0)
+        self.robot = Robot(5.0, 5.0, 0.0, 0.5, 0.5, 0.7, self.plateforme)
+        self.adaptateur = AdaptateurSimu(self.robot)
+        self.strat = AvancerDroit(self.adaptateur, 2.0)
+
+    def test_start(self):
+        self.strat.start()
+        # La stratégie demande V=0.1 et W=0.0
+        # L'adaptateur doit régler les deux roues à 0.1
+        self.assertEqual(self.robot.vL, 0.1)
+        self.assertEqual(self.robot.vR, 0.1)
+
+    def test_stop(self):
+        self.strat.parcouru = 2.0
+        self.assertTrue(self.strat.stop())
 
