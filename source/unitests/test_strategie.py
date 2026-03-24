ea8e9969932245e751934530a928fbfccb6dc553
ichrak33 <bchirichrak5@gmail.com>
2026-03-24 22:43:36 +0100

    tests pour la strategoe TestTourner

--- source/unitests/test_strategie.py
+++ source/unitests/test_strategie.py
@@ -22,3 +22,24 @@ class TestAvancerDroit(unittest.TestCase):
         self.strat.parcouru = 2.0
         self.assertTrue(self.strat.stop())
 
+class TestTourner(unittest.TestCase):
+    def setUp(self):
+        self.plateforme = Plateforme(10.0, 10.0)
+        self.robot = Robot(5.0, 5.0, 0.0, 0.5, 0.5, 0.7, self.plateforme)
+        self.adaptateur = AdaptateurSimu(self.robot)
+        self.strat = Tourner(self.adaptateur, 90)
+
+    def test_start(self):
+        self.strat.start()
+        # La stratégie demande V=0.0 et W=0.1
+        # Avec un empattement (L) de 0.5, l'adaptateur calcule vL=-0.025 et vR=0.025
+        self.assertEqual(self.robot.vL, -0.025)
+        self.assertEqual(self.robot.vR, 0.025)
+
+    def test_stop(self):
+        self.strat.angle_parcouru = math.radians(90)
+        self.assertTrue(self.strat.stop())
+
+if __name__ == '__main__':
+    unittest.main()
+
