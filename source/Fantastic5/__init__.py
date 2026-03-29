f3529e4a1ee60e27cc10f0fb50b2279fc71515d2
Zina Bensadok <bensadokzina964@gmail.com>
2026-03-29 11:47:08 +0200

    modification du init de fantastic : suppression des adaptateurs dans ce fichier comme demandé par le prof

--- source/Fantastic5/__init__.py
+++ source/Fantastic5/__init__.py
@@ -2,21 +2,15 @@ from .graphique import PygameView
 from .simulation import Plateforme, Robot 
 from .strategie import AvancerDroit, Tourner, TracerCarre, Choregraphie 
 
-from .adaptateur import AdaptateurSimu
 
 __all__ = ["PygameView", "Plateforme", "Robot", "AvancerDroit", "Tourner", "TracerCarre", "Choregraphie", "initialisation_simulation"]
 
 def initialisation_simulation():
     """Création  d'une simulation avec des paramètres par défaut pour faire déplacer le robot en carré et le faire avancer dans un plan continu contenant des obstacles dans le main"""
     # Plateforme
-    p = Plateforme(10, 10)
-    p.init_obstacle(8, 3, 2, 1)
-    p.init_obstacle(5, 5, 1, 1)
+    p = Plateforme(10.0, 10.0)
+    p.init_obstacle(2.0, 2.0, 1.0, 1.0) 
+    #Robot 
+    r = Robot(5.0, 5.0, 0.0, 0.5, 0.5, 0.7, p)
     
-    # Robot
-    r = Robot(5, 2, 0, 0.5, 0.5, 0.7, p)
-
-    #adaptateur
-    adp = AdaptateurSimu(r)
-    
-    return p, r,adp
+    return p, r
