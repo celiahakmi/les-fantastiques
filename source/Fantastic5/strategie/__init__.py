dfe0be22b7f41c993ff28cd5e759d5173b1cfba7
diakitekafine7 <diakitekafine@gmail.com>
2026-03-30 19:07:43 +0200

    Update __init__.py

--- source/Fantastic5/strategie/__init__.py
+++ source/Fantastic5/strategie/__init__.py
@@ -1,3 +1,2 @@
-from .strategie import AvancerDroit, Tourner, Choregraphie, ApprocherLeMur , Boucle, Condition
-__all__ = ["AvancerDroit", "Tourner", "Arreter", "Choregraphie", "ApprocherLeMur","Boucle", "Condition"]
-
+from .strategie import AvancerDroit, Tourner, Arreter, Choregraphie, Accelerer , Boucle, Condition
+__all__ = ["AvancerDroit", "Tourner", "Arreter", "Choregraphie", "Accelerer","Boucle","Condition"]
