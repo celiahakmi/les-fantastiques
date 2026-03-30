e1a630697e46c009230b3d62b552fd3783e7118b
diakitekafine7 <diakitekafine@gmail.com>
2026-03-30 16:42:01 +0200

    ajout Arreter __init__.py

--- source/Fantastic5/strategie/__init__.py
+++ source/Fantastic5/strategie/__init__.py
@@ -1,3 +1,3 @@
 from .strategie import AvancerDroit, Tourner, Choregraphie, ApprocherLeMur , Boucle
-__all__ = ["AvancerDroit", "Tourner", "Choregraphie", "ApprocherLeMur","Boucle"]
+__all__ = ["AvancerDroit", "Tourner", "Arreter", "Choregraphie", "ApprocherLeMur","Boucle"]
 
