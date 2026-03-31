1455f91d7bfb533a1de4cf8419fecd7ede0b0239
ichrak33 <bchirichrak5@gmail.com>
2026-03-31 15:08:11 +0200

    commencer a faire la fenêtre et la platforme de l'affichage en 3d

--- source/Fantastic5/graphique/affichage3d.py
+++ source/Fantastic5/graphique/affichage3d.py
@@ -1,2 +1,16 @@
 import math
 from vpython import canvas, box, vector, color, rate
+
+class Vue3D:
+    def __init__(self, plateforme, robot):
+        self.plateforme = plateforme
+        self.robot_physique = robot 
+
+        # La fenêtre
+        self.vu = canvas(title='Simulation 3D', width=800, height=600)
+        self.vu.center = vector(5.0, 0, 5.0) 
+        
+        # Plateforme
+        self.sol = box(pos=vector(5.0, -0.1, 5.0), 
+                       size=vector(10.0, 0.2, 10.0), 
+                       color=color.gray(0.7))
