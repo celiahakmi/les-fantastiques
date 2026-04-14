9429a98b5efdb047c77000e9604c0a75d8dfa9f2
diakitekafine7 <diakitekafine@gmail.com>
2026-04-14 08:58:45 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,5 +1,6 @@
 import time  
 from Fantastic5 import initialisation_simulation 
+from Fantastic5.graphique.affichage3d import Vue3D
 from Fantastic5.adaptateur import AdaptateurSimu, AdaptateurIRL
 from Fantastic5.strategie import AvancerDroit, Tourner,Arreter, Accelerer, Choregraphie, Condition, Boucle
 
@@ -8,15 +9,13 @@ def main():
     simu = True  
 
     if simu:
-        print("Mode simulation activé ")
+        print("mode  3D activé ")
+        from Fantastic5.graphique import Vue3D 
 
-        import pygame
-        from Fantastic5.graphique import PygameView 
-
-        #le monde virtuel
         p, r = initialisation_simulation() 
 
-        view = PygameView(p, r, 50)
+        #  initialise la vue 3D
+        view = Vue3D(p, r)
         adp = AdaptateurSimu(r)
 
     else:
@@ -26,23 +25,16 @@ def main():
         # adp = AdaptateurIRL(r)
         pass 
 
-    def condition_distance(adaptateur):
-        return adaptateur.get_distance() > 1.0
-    
     action1 = Choregraphie(adp, [AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90)])
-    action2 = Condition(adp,condition_distance,Accelerer(adp, 20.0, 0.5, 0.01),Arreter(adp))
+    action2 = Condition(adp, Accelerer(adp, 20.0, 0.5, 0.01), Arreter(adp), 1.0)
     strat_globale = Choregraphie(adp, [action1, action2])
+
     strat_globale.start()
-    # on peut utiliser la stratégie boucle ici en modifiant aussi le while running :
-    #action1 = Choregraphie(adp, [AvancerDroit(adp, 2), Tourner(adp, 90)])
-    #nbRepet = 4
-    #boucle_action = Boucle(adp, action1, n_repetitions=4)
-    #boucle_action.start()
-    
+
     running = True
 
-    while running:
 
+    while running:
         if not strat_globale.stop():
             strat_globale.step()
         else:
@@ -50,18 +42,15 @@ def main():
             if not simu:
                 running = False 
 
+        # on met à jour la physique et la 3D
         if simu:      
+            # Le robot calcule sa nouvelle position
             if not r.update(): 
-                pass # Le robot a touché un mur
+                print("Collision ou arrêt ")
+                running = False 
 
-            view.dessiner()
+            view.dessiner() 
 
-            # fermer la fenêtre 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-        time.sleep(0.001)
         time.sleep(0.01)
 
     if simu:
