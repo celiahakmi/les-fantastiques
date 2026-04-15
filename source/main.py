4403379964dac7214eb7a62fb73c802f8cafd877
InesAddam <inesaddam06@gmail.com>
2026-04-15 11:48:03 +0200

    maj main

--- source/main.py
+++ source/main.py
@@ -2,61 +2,55 @@ import time
 from Fantastic5 import initialisation_simulation 
 from Fantastic5.API import Robot2IN013
 from Fantastic5.adaptateur import AdaptateurSimu, AdaptateurIRL
-from Fantastic5.strategie import AvancerDroit, Tourner,Arreter, Accelerer, Choregraphie, Condition, Boucle
+from Fantastic5.strategie import AvancerDroit, Tourner, Arreter, Accelerer, Choregraphie, Condition
 
 def main():
 
-    simu = True  
+    simu = False
 
     if simu:
-        print("mode  3D activé ")
-        from Fantastic5.graphique import Vue3D 
-
-        p, r, _ = initialisation_simulation() 
-
-        #  initialise la vue 3D
-        view = Vue3D(p, r)
+        print("Mode simulation activé")
+        _, r, _ = initialisation_simulation()
         adp = AdaptateurSimu(r)
-
     else:
-        from Fantastic5.API.robotAPI import Robot2IN013
-        _, _, r2 = initialisation_simulation() 
-
-        print("Mode robot réel activé ")
+        print("Mode robot réel activé")
+        r2 = Robot2IN013(nb_img=5, fps=25)
         adp = AdaptateurIRL(r2)
-        pass 
 
-    action1 = Choregraphie(adp, [AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90)])
+    # Définition de la stratégie globale
+    action1 = Choregraphie(adp, [
+        AvancerDroit(adp, 2), Tourner(adp, 90),
+        AvancerDroit(adp, 2), Tourner(adp, 90),
+        AvancerDroit(adp, 2), Tourner(adp, 90),
+        AvancerDroit(adp, 2), Tourner(adp, 90)
+    ])
+    
     action2 = Condition(adp, Accelerer(adp, 20.0, 0.5, 0.01), Arreter(adp), 1.0)
+    
     strat_globale = Choregraphie(adp, [action1, action2])
 
     strat_globale.start()
 
     running = True
 
-
     while running:
         if not strat_globale.stop():
             strat_globale.step()
         else:
-            adp.set_vitesse(0.0, 0.0) 
-            if not simu:
-                running = False 
+            adp.set_vitesse(0.0, 0.0)
+            running = False
 
-        # on met à jour la physique et la 3D
-        if simu:      
-            # Le robot calcule sa nouvelle position
-            if not r.update(): 
-                print("Collision ou arrêt ")
-                running = False 
-
-            view.dessiner() 
+        # Mise à jour simulation si nécessaire
+        if simu:
+            if not r.update():
+                print("Collision ou arrêt détecté dans la simulation")
+                running = False
 
         time.sleep(0.01)
 
-    if simu:
-        import pygame
-        pygame.quit()
+    # Arrêt du robot réel
+    if not simu:
+        adp.set_vitesse(0.0, 0.0)
 
 if __name__ == "__main__":
     main()
