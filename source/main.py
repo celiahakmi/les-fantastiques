2460a8b879bb52bc0bca42b997f6f69cd63afec3
ichrak33 <bchirichrak5@gmail.com>
2026-04-15 09:51:24 +0200

    la version adapté a l'affichage 2D

--- source/main.py
+++ source/main.py
@@ -1,40 +1,41 @@
 import time  
 from Fantastic5 import initialisation_simulation 
-from Fantastic5.graphique.affichage3d import Vue3D
 from Fantastic5.adaptateur import AdaptateurSimu, AdaptateurIRL
 from Fantastic5.strategie import AvancerDroit, Tourner,Arreter, Accelerer, Choregraphie, Condition, Boucle
 
 def main():
-
+    
     simu = True  
-
+    
     if simu:
-        print("mode  3D activé ")
-        from Fantastic5.graphique import Vue3D 
-
+        print("Mode simulation activé ")
+  
+        import pygame
+        from Fantastic5.graphique import PygameView 
+        
+        #le monde virtuel
         p, r = initialisation_simulation() 
-
-        #  initialise la vue 3D
-        view = Vue3D(p, r)
+        
+        view = PygameView(p, r, 50)
         adp = AdaptateurSimu(r)
-
+        
     else:
         print("Mode robot réel activé ")
         # Ici on crée le vrai robot 
         # r =
         # adp = AdaptateurIRL(r)
         pass 
-
+    
     action1 = Choregraphie(adp, [AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90), AvancerDroit(adp, 2), Tourner(adp, 90)])
     action2 = Condition(adp, Accelerer(adp, 20.0, 0.5, 0.01), Arreter(adp), 1.0)
     strat_globale = Choregraphie(adp, [action1, action2])
-
+    
     strat_globale.start()
-
+    
     running = True
 
-
     while running:
+        
         if not strat_globale.stop():
             strat_globale.step()
         else:
@@ -42,15 +43,17 @@ def main():
             if not simu:
                 running = False 
 
-        # on met à jour la physique et la 3D
         if simu:      
-            # Le robot calcule sa nouvelle position
             if not r.update(): 
-                print("Collision ou arrêt ")
-                running = False 
+                pass # Le robot a touché un mur
+                
+            view.dessiner()
 
-            view.dessiner() 
-
+            # fermer la fenêtre 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    
         time.sleep(0.01)
 
     if simu:
